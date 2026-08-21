aa82bb9b0ed4830b735cf5eccfe27ec471414acc
Mtths <matthias.ertl@gmail.com>
2026-08-21 15:05:13 +0200

    Show Codex weekly and three Cursor limits

--- include/providers/UsageProvider.h
+++ include/providers/UsageProvider.h
@@ -5,7 +5,7 @@
 struct UsageWindow { String label; float usedPercent = -1; String resetText; };
 struct UsageSnapshot {
   String provider; bool ok = false; String status; String plan; String updated;
-  UsageWindow primary; UsageWindow secondary; float credits = -1;
+  UsageWindow primary; UsageWindow secondary; UsageWindow tertiary; float credits = -1;
 };
 
 class UsageProvider {
